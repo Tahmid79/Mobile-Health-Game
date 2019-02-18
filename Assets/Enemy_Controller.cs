013507602fbc47cef4fe96de0eaa8bcde03b145a
Tahmid Farhan <tahmidfarhan12@gmail.com>
2019-02-18 16:23:40 +0800

    Update Enemy_Controller.cs

--- Assets/Enemy_Controller.cs
+++ Assets/Enemy_Controller.cs
@@ -9,8 +9,8 @@ public class Enemy_Controller : MonoBehaviour {
 
     Transform target;
     NavMeshAgent agent;
-   
 
+   
     // Use this for initialization
     void Start () {
         target = PlayerManager.instance.player.transform;
@@ -22,9 +22,20 @@ public class Enemy_Controller : MonoBehaviour {
         float distance;
         distance = Vector3.Distance(target.position, transform.position);
 
-        if(distance <= lookRadius)
+        if(distance <= lookRadius && distance>2f )
         {
-            agent.SetDestination(target.position);
+            transform.LookAt(target.position);
+            transform.Rotate(new Vector3(0, -90, 0), Space.Self);
+
+
+            //agent.SetDestination(target.position);
+            float speed = 4f;
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+
+        if(distance < 2f)
+        {     
+            BackAway();     
         }
 		
 	}
@@ -34,4 +45,11 @@ public class Enemy_Controller : MonoBehaviour {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
+
+    void BackAway()
+    {
+        transform.Translate(Vector3.back * 4f * Time.deltaTime);
+    }
+
+
 }
