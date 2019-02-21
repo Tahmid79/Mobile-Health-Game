f526126d5338a312a399521e24c28d7e80ee0204
Usama Zidan <osama.zadan@gmail.com>
2019-02-21 14:50:45 +0800

    Enemy Now follows player smoothly

--- Assets/Enemy_Controller.cs
+++ Assets/Enemy_Controller.cs
@@ -6,15 +6,16 @@ using UnityEngine.AI;
 public class Enemy_Controller : MonoBehaviour {
 
     public float lookRadius = 5f;
+    public Rigidbody rb;
 
      public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
-    public int attackDamage = 5;               // The amount of health taken away per attack.
+    public int attackDamage = 1;               // The amount of health taken away per attack.
 
 
-    Animator anim; 
-   public Animator Eanim;                             // Reference to the animator component.
+    Animator anim;                              // Reference to the animator component.
+     public Animator Eanim;                             // Reference to the animator component.
     GameObject Walking;  
-    GameObject Enemy     ;                   // Reference to the player GameObject.
+    GameObject Enemy ;                          // Reference to the player GameObject.
     PlayerHealth playerHealth;                  // Reference to the player's health.
     bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
     float timer;
@@ -22,40 +23,49 @@ public class Enemy_Controller : MonoBehaviour {
 
 
     Transform target;
-    NavMeshAgent agent;
-   
 
+   
     // Use this for initialization
     void Start () {
-        
-        Enemy  =  GameObject.FindGameObjectWithTag ("Enemy");
-        Eanim = Enemy.GetComponent<Animator> (); 
-        
         target = PlayerManager.instance.player.transform;
         Walking = GameObject.FindGameObjectWithTag ("Player");
-
         playerHealth = Walking.GetComponent <PlayerHealth> ();
-        agent = GetComponent<NavMeshAgent>();
+        Enemy  =  GameObject.FindGameObjectWithTag ("Enemy");
+        Eanim = Enemy.GetComponent<Animator> ();
 	}
-	
-	// Update is called once per frame
+//The target player
+ public Transform player;
+ 
+ //In what time will the enemy complete the journey between its position and the players position
+ public float smoothTime = 2f;
+ //Vector3 used to store the velocity of the enemy
+ private Vector3 smoothVelocity = Vector3.zero;
+ //Call every frame
 	void Update () {
-                timer += Time.deltaTime;
+        timer += Time.deltaTime;
 
-        float distance;
-        distance = Vector3.Distance(target.position, transform.position);
+        //Look at the player
+     transform.LookAt(player);
+     //Calculate distance between player
+     float distance = Vector3.Distance(transform.position, player.position);
+     //If the distance is smaller than the walkingDistance
+        if(distance <= lookRadius && distance>2f )
+      {
+//Move the enemy towards the player with smoothdamp
+         transform.position = Vector3.SmoothDamp(transform.position, player.position, ref smoothVelocity, smoothTime);
+            Eanim.Play("Running");
+        }else if(distance <= 2f)
+        {     
+           //    BackAway();  
+          rb.velocity = new Vector3(0, 0, 0);
 
-        if(distance <= lookRadius)
+
+            Attack();
+
+        }else
         {
-            agent.SetDestination(target.position);
-                        Eanim.Play("Running");
+            Eanim.Play("Idle");
 
-            if(timer >= timeBetweenAttacks){
-                // ... attack.
-          Eanim.Play("Take 001");
-
-            Attack ();
-            }
         }
 		
 	}
@@ -69,6 +79,8 @@ public class Enemy_Controller : MonoBehaviour {
         
             // ... damage the player.
             playerHealth.TakeDamage (attackDamage);
+                                    Eanim.Play("Take 001");
+
         
     }
 
@@ -77,4 +89,11 @@ public class Enemy_Controller : MonoBehaviour {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, lookRadius);
     }
+
+    void BackAway()
+    {
+        transform.Translate(Vector3.back * 4f * Time.deltaTime);
+    }
+
+
 }
