cca14c82da65834bcac14bcf327baec15a4ab404
Tahmid Farhan <tahmidfarhan12@gmail.com>
2019-02-21 21:10:48 +0800

    Revert "Chase Player and Attack"
    This reverts commit 6da4bb9a94b9701bee58d00e3f32de62da7dcf57, reversing
    changes made to a5a1fab4985ab510acfc0726f437ec9d56b99c7a.

--- Assets/Enemy_Controller.cs
+++ Assets/Enemy_Controller.cs
@@ -5,21 +5,14 @@ using UnityEngine.AI;
 
 public class Enemy_Controller : MonoBehaviour {
 
-<<<<<<< HEAD
     public float lookRadius = 10f;
-=======
-    public float lookRadius = 5f;
-    public Rigidbody rb;
->>>>>>> USama
 
      public float timeBetweenAttacks = 0.5f;     // The time in seconds between each attack.
-    public int attackDamage = 1;               // The amount of health taken away per attack.
+    public int attackDamage = 5;               // The amount of health taken away per attack.
 
 
     Animator anim;                              // Reference to the animator component.
-     public Animator Eanim;                             // Reference to the animator component.
-    GameObject Walking;  
-    GameObject Enemy ;                          // Reference to the player GameObject.
+    GameObject Walking;                          // Reference to the player GameObject.
     PlayerHealth playerHealth;                  // Reference to the player's health.
     bool playerInRange;                         // Whether player is within the trigger collider and can be attacked.
     float timer;
@@ -27,52 +20,27 @@ public class Enemy_Controller : MonoBehaviour {
 
 
     Transform target;
-<<<<<<< HEAD
     NavMeshAgent agent;
 
     bool backing;
     int flg = 0;
 
-=======
->>>>>>> USama
 
-   
     // Use this for initialization
     void Start () {
         target = PlayerManager.instance.player.transform;
         Walking = GameObject.FindGameObjectWithTag ("Player");
         playerHealth = Walking.GetComponent <PlayerHealth> ();
-        Enemy  =  GameObject.FindGameObjectWithTag ("Enemy");
-        Eanim = Enemy.GetComponent<Animator> ();
+        agent = GetComponent<NavMeshAgent>();
 	}
-//The target player
- public Transform player;
- 
- //In what time will the enemy complete the journey between its position and the players position
- public float smoothTime = 2f;
- //Vector3 used to store the velocity of the enemy
- private Vector3 smoothVelocity = Vector3.zero;
- //Call every frame
+	
+	// Update is called once per frame
 	void Update () {
-        timer += Time.deltaTime;
+                timer += Time.deltaTime;
 
-        //Look at the player
-     transform.LookAt(player);
-     //Calculate distance between player
-     float distance = Vector3.Distance(transform.position, player.position);
-     //If the distance is smaller than the walkingDistance
-        if(distance <= lookRadius && distance>2f )
-      {
-//Move the enemy towards the player with smoothdamp
-         transform.position = Vector3.SmoothDamp(transform.position, player.position, ref smoothVelocity, smoothTime);
-            Eanim.Play("Running");
-        }else if(distance <= 2f)
-        {     
-           //    BackAway();  
-          rb.velocity = new Vector3(0, 0, 0);
+        float distance;
+        distance = Vector3.Distance(target.position, transform.position);
 
-
-<<<<<<< HEAD
 
         
 
@@ -95,14 +63,6 @@ public class Enemy_Controller : MonoBehaviour {
             }
             backing = false;
           
-=======
-            Attack();
-
-        }else
-        {
-            Eanim.Play("Idle");
-
->>>>>>> USama
         }
 
         
@@ -118,8 +78,6 @@ public class Enemy_Controller : MonoBehaviour {
         
             // ... damage the player.
             playerHealth.TakeDamage (attackDamage);
-                                    Eanim.Play("Take 001");
-
         
     }
 
@@ -134,7 +92,6 @@ public class Enemy_Controller : MonoBehaviour {
         transform.Translate(Vector3.back * 4f * Time.deltaTime);
     }
 
-<<<<<<< HEAD
     void MoveTowards()
     {
         transform.LookAt(target.position);
@@ -150,7 +107,3 @@ public class Enemy_Controller : MonoBehaviour {
 
 
 }
-=======
-
-}
->>>>>>> USama
