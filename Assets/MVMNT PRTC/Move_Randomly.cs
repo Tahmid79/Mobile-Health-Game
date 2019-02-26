d1ea2dbbd3d2566e7e690aa84391939c8a548079
Tahmid Farhan <tahmidfarhan12@gmail.com>
2019-02-26 22:29:28 +0800

    Target Follow

---
+++ Assets/MVMNT PRTC/Move_Randomly.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Move_Randomly : MonoBehaviour {
+
+    NavMeshAgent navMeshAgent;
+    public float timerForNewpath;
+
+
+
+	// Use this for initialization
+	void Start () {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    Vector3 getnewrandomposition()
+    {
+        float x = Random.Range(-20, 20);
+        float z = Random.Range(-20, 20);
+
+        Vector3 pos = new Vector3(x, 0, z);
+
+        return pos;
+    }
+
+
+}
