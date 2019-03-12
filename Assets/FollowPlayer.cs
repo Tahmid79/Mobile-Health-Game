a8f3d055ef65f21ea4b8e463f0278747a4b4c2ae
Tahmid79 <45458895+Tahmid79@users.noreply.github.com>
2019-03-12 20:19:21 +0800

    Camera Rotate

--- Assets/FollowPlayer.cs
+++ Assets/FollowPlayer.cs
@@ -6,14 +6,21 @@ public class FollowPlayer : MonoBehaviour {
 
 	public Transform player;
 	public Vector3 offset;
+	Vector3 init_pos;
+	
 
 	// Use this for initialization
 	void Start () {
-		
+	  init_pos = player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.position = player.position + offset;
+		// transform.position = new Vector3(player.position.x , init_pos.y, player.position.z) + offset;
+
+
+		// transform.rotation = player.transform.rotation  ;
+		//transform.Rotate(Vector3.up, 10f * Time.deltaTime);
+		
 	}
 }
